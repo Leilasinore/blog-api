9c4d98511647533fac6facd17a020a5552b8ad7c
Leila Sinore <leilasinore99@gmail.com>
2023-11-16 11:51:39 +0300

    registered models in admin and database connection

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'posts.apps.PostsConfig'
 ]
 
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'myblog',
+        'HOST': 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : 'qwerty@123'
     }
 }
 
