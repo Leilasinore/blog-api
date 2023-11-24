dcfc5470867fa679767a4c0700b9a34ba3a7b6c6
Leila Sinore <leilasinore99@gmail.com>
2023-11-24 22:56:25 +0300

    switched to generic class based views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("articles/",views.Articles.as_view()),
-    path("articles/<int:id>/",views.myarticle,name="myarticle")
+    path("articles/",views.Articles.as_view(),name="Articles"),
+    path("articles/<int:id>/",views.MyArticle.as_view(),name="MyArticle")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,86 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin,UpdateModelMixin,DestroyModelMixin,ListModelMixin,RetrieveModelMixin
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import BlogPost,Category
 from .serializers import BlogpostSerializer
 
-class Articles(APIView):
-    def get(self,request):
-        queryset = BlogPost.objects.select_related('category').all()
-        serializer = BlogpostSerializer(queryset,many=True)
-        return Response(serializer.data)
-    
-    def post(self,request):
-        #steps 
-        #deserialize the data
-        #validate the data
-        #save the data
-        serializer = BlogpostSerializer(data=request.data)
-        #do data validation
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        # serializer.validated_data
-        #the restful convention is to return the created object and a status code of 201
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
-class MyArticle(APIView):
-    article = get_object_or_404(BlogPost,pk=id)
-    def get(self,request,id):
-        serializer = BlogpostSerializer(self.article)
-        return Response(serializer.data)
+
+#implementation of views using GenericAPIView class and mixins
+class Articles(GenericAPIView,ListModelMixin,CreateModelMixin):
+    queryset = BlogPost.objects.select_related('category').all()
+    serializer = BlogpostSerializer(queryset,many=True)
+    def get(self,request,*args, **kwargs):
+        return self.list(request,*args, **kwargs)
     
-    def put(self,request,id):
-        serializer = BlogpostSerializer(self.article,data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    def post(self,request,*args, **kwargs):
+        return self.create(request,*args, **kwargs)
+
+
+
+class MyArticle(GenericAPIView,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin):
+    article = get_object_or_404(BlogPost,pk=id)
+    serializer = BlogpostSerializer(article)
+    def get(self,request ,*args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self,request,*args, **kwargs):
+        return self.update(request,*args, **kwargs)
+    
+    def delete(self,request,*args, **kwargs):
+        return self.destroy(request,*args, **kwargs)
+    
+
+
+
+
+
+
+
+
+#implementation of views using APIView class
+# class Articles(APIView):
+#     def get(self,request):
+#         queryset = BlogPost.objects.select_related('category').all()
+#         serializer = BlogpostSerializer(queryset,many=True)
+#         return Response(serializer.data)
+    
+#     def post(self,request):
+#         #steps 
+#         #deserialize the data
+#         #validate the data
+#         #save the data
+#         serializer = BlogpostSerializer(data=request.data)
+#         #do data validation
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         # serializer.validated_data
+#         #the restful convention is to return the created object and a status code of 201
+#         return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+# class MyArticle(APIView):
+#     def get(self,request,id):
+#         article = get_object_or_404(BlogPost,pk=id)
+#         serializer = BlogpostSerializer(article)
+#         return Response(serializer.data)
+    
+#     def put(self,request,id):
+#         article = get_object_or_404(BlogPost,pk=id)
+#         serializer = BlogpostSerializer(article,data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
         
 
+
+
+
+#implementation of views using function based views
 # @api_view(['GET', 'POST'])
 # def articles(request):
 #     if request.method == 'GET':
